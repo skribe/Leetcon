bdc8ad34ab9b49de20fbb884facdb149dc994417
skribe <skribeproductions@gmail.com>
2024-02-18 16:41:35 +0800

    Basic stuff

--- Leet-converter.py
+++ Leet-converter.py
@@ -3,3 +3,46 @@
 #imports
 from sys import exit
 
+def main():
+    
+
+def leet_encoder(unleet_word):
+    char_map = {
+        "a": ["4","@","^", "/\\"],
+        "b": ["8", "|3"],
+        "c": ["[", "(", "<"],
+        "d": [")", "|)"],
+        "e": ["3", "[-"],
+        "f": ["ph", "|=", "v"],
+        "g": ["6", "9", "C-"],
+        "h": ["#", "|-|"],
+        "i": ["|", "!"],
+        "j": ["]", "_|"],
+        "k": ["|<", "|c"],
+        "l": ["|_", "1"],
+        "m": ["nn", "11", "(v)"],
+        "n": ["|\|", "^/"],
+        "o": ["0", "()"],
+        "p": ["|*", "|>"],
+        "q": ["2", "0_"],
+        "r": ["I2", "|?"],
+        "s": ["5", "$", "z"],
+        "t": ["7", "+"],
+        "u": ["v", "|_|"],
+        "v": ["\/", "|/"],
+        "w": ["uu", "vv"],
+        "x": ["][", ")("],
+        "y": ["j", "`/"],
+        "z": ["7_", "s"],
+        }
+
+    leet_word = ""
+
+    for c in unleet_word:
+        if random() <= 0.30:
+            leet_letter = choice(char_map[c.lower()])
+            leet_word = leet_word + leet_letter
+        else:
+            leet_word = leet_word + c
+    
+    return leet_word
